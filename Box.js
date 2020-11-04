4cbc7444644ff30cb91845da060fb51890a37628
unknowhaker <70791877+unknowhaker@users.noreply.github.com>
2020-11-04 11:26:30 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,23 @@
+class Box {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.8,
+          'friction':1,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(World, this.body);
+    }
+    display(){
+      
+      stroke ("green");
+      strokeWeight (4);
+      rectMode(CENTER);
+      fill(255);
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };
